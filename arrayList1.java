174ec2acd7347fcd6681ba93bd8baa781d18a71d
Laxmi-mungal <60098134+Laxmi-mungal@users.noreply.github.com>
2020-01-22 11:32:31 +0530

    Add files via upload
     Write a Java program to create a new array list, add some colors (string) and print out the collection.

---
+++ arrayList1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class arrayList1 {
+  public static void main(String[] args) {
+
+
+  List<String> list = new ArrayList<String>();
+
+  list.add("Red");
+
+  list.add("Green");
+  list.add("Orange");
+  list.add("White");
+  list.add("Black");
+  System.out.println(list);
+ }
+}
+
+
